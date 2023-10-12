3ddec01b8cee6206e992005bea59edb568fbb066
Nitish Lobo <nitishlobo@gmail.com>
2023-10-13 01:51:35 +1000

    refactor: remove application submount and instead do a router for v1

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 """Main app for math quiz."""
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
-from v1.multiplication import router as multiplication_router
+from v1.multiplication.router import router as multiplication_router
 from v1.multiplication.service import generate_times_table_grid
 from v1.operands.schemas import Operand, Operands
-from v1.ping import router as ping_router
+from v1.ping.router import router as ping_router
 
 # Main app and versions
 main_app = FastAPI()
-app_v1 = FastAPI()
-
-# Main app routes
-main_app.include_router(ping_router.router)
 
 # API v1 routes
-app_v1.include_router(ping_router.router)
-app_v1.include_router(multiplication_router.router)
+v1_router = APIRouter(prefix="/api/v1")
+v1_router.include_router(ping_router)
+v1_router.include_router(multiplication_router)
 
-# Attach API v1 to main app
-main_app.mount("/v1", app_v1)
+# Main app routes
+main_app.include_router(ping_router)
+main_app.include_router(v1_router)
 
 
 if __name__ == "__main__":
