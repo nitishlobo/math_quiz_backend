9ac474b65e77ea3196871936c805b4a4adf79953
Nitish Lobo <nitishlobo@gmail.com>
2024-05-04 03:29:54 +1000

    feat: populate lifespan state variables through the app rather than yielding it

--- v1/lifespans.py
+++ v1/lifespans.py
@@ -1,37 +1,27 @@
 """FastAPI lifespan events."""
 
-from collections.abc import AsyncGenerator, AsyncIterator
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
-from typing import TypedDict
 
 from fastapi import FastAPI
-from sqlalchemy.engine import Connection
 
 from v1.database.connections import create_database_connection
 from v1.settings import DEBUG_DATABASE, db_info
 
 
-class LifespanStates(TypedDict):
-    """API lifespan states."""
-
-    # http_client: httpx.AsyncClient
-    db_connection: Connection
-
-
 @asynccontextmanager
-async def database_connection_lifespan(_app: FastAPI) -> AsyncGenerator[Connection, None]:
+async def database_connection_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     """Attach a database connection to the FastAPI application."""
     with create_database_connection(db_url=db_info.url, debug_db=DEBUG_DATABASE) as db_connection:
-        yield db_connection
+        app.state.db_connection = db_connection
+        yield
 
 
 @asynccontextmanager
-async def lifespans(app: FastAPI) -> AsyncIterator[LifespanStates]:
+async def lifespans(app: FastAPI) -> AsyncGenerator[None, None]:
     """Attach all lifespans to the FastAPI application.
 
     See https://fastapi.tiangolo.com/advanced/events/ for details.
     """
-    # async with database_connection_lifespan(app) as db_connection, httpx.AsyncClient() as client:
-    #     yield {"http_client": client, "db_connection": db_connection}
-    async with database_connection_lifespan(app) as db_connection:
-        yield {"db_connection": db_connection}
+    async with database_connection_lifespan(app):
+        yield
